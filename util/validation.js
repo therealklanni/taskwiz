cc5229a0373d19401c2a84bb40dc18479fe41bb8
Kevin Lanni <therealklanni@gmail.com>
2016-01-13 20:36:18 -0700

    add description validator

--- util/validation.js
+++ util/validation.js
@@ -55,9 +55,11 @@ export const durations = [
 export const durationRegExp = new RegExp(`^([-]?\d)?(${durations.join('|')})$`)
 export const maskRegExp = /^[-+XW]+$/
 export const dateRegExp = /^(19[7-9]\d|[2-9]\d{3})(0[1-9]|1[0-2])([0-2]\d|3[01])T([01]\d|2[0-3])([0-5]\d){2}Z$/
+export const descriptionRegExp = /\n/g
 export const priorityRegExp = /^(L|M|H)$/
 
 export const validateDuration = duration => (durationRegExp).test(duration)
 export const validateMask = mask => (maskRegExp).test(mask)
 export const validateDate = date => (dateRegExp).test(date)
+export const validateDescription = desc => !(descriptionRegExp).test(desc)
 export const validatePriority = priority => (priorityRegExp).test(priority)
